45e711d68c3f73073d6848cf026bc168f423fdd6
student33 <student33@sit1.opsta.in.th>
2021-03-03 13:41:01 +0700

    Create all error

--- 62130500033_Groupwork_6/profile.js
+++ 62130500033_Groupwork_6/profile.js
@@ -1,20 +1,86 @@
-    const app = {
-        data() {
-            return {
-                url:"./images/62130500033.jpg",
-                firstname:"Thanapat",
-                lastname:"Kallakul",
-                position:"Scholar",
-                article:"Article",
-                article_no:"34",
-                followers:"Followers",
-                followers_no:"940",
-                ratings:"Ratings",
-                ratings_no:"8.9",
-                chats:"Chat",
-                width:"150"
-            }
+const constraints = {
+    firstname: {
+        presence: {message: "is required"},
+        format: {
+            pattern: "[a-z]+",
+            message: "can only contain a-z"
         }
-
+    },
+    lastname: {
+        presence:{message: "is required"},
+        format: {
+            pattern: "[a-z]+",
+            message: "can only contain a-z"
+        }
+    },
+    nickname: {
+        presence:{message: "is required"},
+        format: {
+            pattern: "[a-z]+",
+            message: "can only contain a-z"
+        }
+    },
+    age: {
+        presence: true,
+        numericality: {
+            lessThan: 150,
+            notValid:"must be integer"
+        }
+    },
+    chosensubject: {
+        presence: true
     }
-    mountedApp = Vue.createApp(app).mount('#app')
+}
+const app = {
+    data() {
+        return {
+            url: "./images/62130500033.jpg",
+            Firstname: null,
+            Lastname: null,
+            Age: null,
+            Education: null,
+            Nickname: null,
+            Chosensubject:"",
+            subject_lists: [{
+                    subject_id: 1,
+                    subject_name: 'Intro to Vue 3'
+                },
+                {
+                    subject_id: 2,
+                    subject_name: 'Vue 3 Forms'
+                },
+                {
+                    subject_id: 3,
+                    subject_name: 'Vue 3 Reactivity'
+                }
+            ],
+            errors: null
+        }
+    },
+    methods: {
+        checkForm(e){
+            this.errors = validate({firstname: this.Firstname,
+                                    lastname: this.Lastname,
+                                    age: this.Age,
+                                    nickname:this.Nickname,
+                                    chosensubject: this.Chosensubject},
+                                    constraints)
+            if(this.errors){
+                e.preventDefault();
+            }
+            else{
+                alert("You profile is updated successful")
+            }
+        },
+        submit(){
+            this.Firstname=this.firstname
+            this.Lastname=this.lastname
+            this.Nickname=this.nickname
+            this.Age=this.age
+            this.Chosensubject=this.chosensubject
+        }
+    }
+
+
+}
+mountedApp = Vue.createApp(app).mount('#app')
